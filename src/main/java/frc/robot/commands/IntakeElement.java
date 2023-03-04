8881d732b95ed8cc02483815002d6868d432742a
TechMaker Robotics <sesi.techmakerrobotics@gmail.com>
2023-03-04 15:53:28 -0300

    configurados botoes

--- src/main/java/frc/robot/commands/IntakeElement.java
+++ src/main/java/frc/robot/commands/IntakeElement.java
@@ -5,12 +5,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.SuperiorIntake;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class IntakeElement extends InstantCommand {
   public IntakeElement() {
+    addRequirements(SuperiorIntake);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
