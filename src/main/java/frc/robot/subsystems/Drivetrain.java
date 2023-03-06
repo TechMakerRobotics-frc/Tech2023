2585c0e44d23c39f70bc2f839a97f2e2d1f3ba8e
TechMaker Robotics <sesi.techmakerrobotics@gmail.com>
2023-03-05 23:14:12 -0300

    Drivetrain atualizado

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,19 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetrainConstants;
@@ -20,14 +33,48 @@ public class Drivetrain extends SubsystemBase {
   private CANSparkMax motorLeftRear = new CANSparkMax(DrivetrainConstants.kMotorLeftRear, MotorType.kBrushless);
   private CANSparkMax motorRightFront = new CANSparkMax(DrivetrainConstants.kMotorRightFront, MotorType.kBrushless);
   private CANSparkMax motorRightRear = new CANSparkMax(DrivetrainConstants.kMotorRightRear, MotorType.kBrushless);
-
+  MotorControllerGroup m_leftMotor = new MotorControllerGroup(motorLeftFront, motorLeftRear);
+  MotorControllerGroup m_rightMotor = new MotorControllerGroup(motorRightFront, motorRightRear);
+  // Set up the differential drive controller
+  private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   // Encoders. São utilizados os enconders internos dos motores
   // Para maior precisão é usada a media dos dois motores
   private RelativeEncoder leftEncoder1, leftEncoder2;
   private RelativeEncoder rightEncoder1, rightEncoder2;
 
   // Placa de navegação
-  private AHRS navX;
+  private AHRS m_gyro;
+  // Set up the BuiltInAccelerometer
+  private final BuiltInAccelerometer m_accelerometer = new BuiltInAccelerometer();
+
+  // Odometry class for tracking robot pose
+  private final DifferentialDriveOdometry m_odometry;
+
+  // Show a field diagram for tracking odometry
+  private final Field2d m_field2d = new Field2d();
+
+  private DifferentialDrivePoseEstimator m_poseEstimator;
+
+  // Show a field diagram for tracking Pose estimation
+  private final Field2d m_estimatedField2d = new Field2d();
+
+  // Create a slew rate filter to give more control over the speed from the
+  // joystick
+  private final SlewRateLimiter m_filter = new SlewRateLimiter(0.5);
+  private final SlewRateLimiter m_filter_turn = new SlewRateLimiter(0.5);
+
+  // Used to put data onto Shuffleboard
+  private ShuffleboardTab driveTab = Shuffleboard.getTab("Drivetrain");
+
+  private GenericEntry m_leftVolts = driveTab.add("Left Volts", 0)
+      .withWidget(BuiltInWidgets.kGraph)
+      .withPosition(3, 3)
+      .getEntry();
+
+  private GenericEntry m_rightVolts = driveTab.add("Right Volts", 0)
+      .withWidget(BuiltInWidgets.kGraph)
+      .withPosition(3, 3)
+      .getEntry();
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
@@ -45,21 +92,6 @@ public class Drivetrain extends SubsystemBase {
     motorLeftFront.setInverted(true);
     motorLeftRear.setInverted(true);
 
-    // Configuro a rampa de aceleração dos modulos
-    //Foi feita uma rampa manual - 05/03/2023 - 20:10
-    /*motorLeftFront.setOpenLoopRampRate(DrivetrainConstants.kRampRate);
-    motorLeftRear.setOpenLoopRampRate(DrivetrainConstants.kRampRate);
-    motorRightFront.setOpenLoopRampRate(DrivetrainConstants.kRampRate);
-    motorRightRear.setOpenLoopRampRate(DrivetrainConstants.kRampRate);*/
-
-    // Limito a corrente dos motores por software para não desligar os fusiveis
-    /*
-     * motorLeftFront.setSmartCurrentLimit(DrivetrainConstants.kMotorCurrentLimit);
-     * motorLeftRear.setSmartCurrentLimit(DrivetrainConstants.kMotorCurrentLimit);
-     * motorRightFront.setSmartCurrentLimit(DrivetrainConstants.kMotorCurrentLimit);
-     * motorRightRear.setSmartCurrentLimit(DrivetrainConstants.kMotorCurrentLimit);
-     */
-
     // Busco o objeto encoder de cada modulo e associo
     leftEncoder1 = motorLeftRear.getEncoder();
     rightEncoder1 = motorRightRear.getEncoder();
@@ -73,16 +105,32 @@ public class Drivetrain extends SubsystemBase {
     rightEncoder2.setPositionConversionFactor(1);
 
     // Seto a posição para no começo
-    resetEncoder();
+    resetEncoders();
 
     // Associo a placa de navegação e reseto ela
-    navX = new AHRS(DrivetrainConstants.NAVX_PORT);
+    m_gyro = new AHRS(DrivetrainConstants.NAVX_PORT);
     resetYaw();
+    Pose2d initialPose = new Pose2d(0, 1.5, m_gyro.getRotation2d());
+    m_field2d.setRobotPose(initialPose);
+
+    m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d(),
+        getLeftDistanceMeters(), getRightDistanceMeters(),
+        initialPose);
+
+    m_poseEstimator = new DifferentialDrivePoseEstimator(DrivetrainConstants.kDriveKinematics,
+        m_gyro.getRotation2d(),
+        getLeftDistanceMeters(), getRightDistanceMeters(),
+        initialPose,
+        VecBuilder.fill(0.02, 0.02, 0.01),
+        VecBuilder.fill(0.1, 0.1, 0.1));
+
+    SmartDashboard.putData("field", m_field2d);
+    SmartDashboard.putData("fieldEstimate", m_estimatedField2d);
 
   }
 
   // Função principal, movimenta o robo para frente e com curva
-  public void setDriveMotors(double forward, double turn, boolean reduce) {
+  public void setDriveMotors(double forward, double turn) {
     SmartDashboard.putNumber("Potencia Frente (%)", forward * 100.0);
     SmartDashboard.putNumber("Potencia Curva (%)", turn * 100.0);
 
@@ -91,21 +139,57 @@ public class Drivetrain extends SubsystemBase {
      */
     double left = forward - turn;
     double right = forward + turn;
-    setTankMotors(left, right, reduce);
+    setTankMotors(left, right);
 
   }
 
+  /**
+   * Resets the odometry to the specified pose
+   * 
+   * @param pose The pose to which to set the odometry
+   */
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    zeroHeading();
+    m_odometry.resetPosition(m_gyro.getRotation2d(),
+        getLeftDistanceMeters(), getRightDistanceMeters(),
+        pose);
+  }
+
+  /**
+   * Sets the max output of the drive. Useful for scaling the drive to drive more
+   * slowly
+   * 
+   * @param maxOutput The maximum output to which the drive will be constrained
+   */
+  public void setMaxOutput(double maxOutput) {
+    m_diffDrive.setMaxOutput(maxOutput);
+  }
+
+  /**
+   * Zeroes the heading of the robot
+   */
+  public void zeroHeading() {
+    m_gyro.reset();
+  }
+
+  /** Reset the gyro. */
+  public void resetGyro() {
+    m_gyro.reset();
+  }
+
   // Movimento em modo de tanque. Util para autonomo e equilibrio
-  public void setTankMotors(double left, double right, boolean reduce) {
+  public void setTankMotors(double left, double right) {
 
     SmartDashboard.putNumber("Potencia Esquerda (%)", left * 100.0);
     SmartDashboard.putNumber("Potencia Direita (%)", right * 100.0);
 
     /*
      * Rotina de rampa
-     * A cada 20ms o defaultComand passa por essa rotina. Então o incremento  
-     * da rampa  é feito com base em um valor constante e não colocando diretamente 
-     * o valor dos  controles nas saidas. Assim tanto as aceleradas quanto as frenagens
+     * A cada 20ms o defaultComand passa por essa rotina. Então o incremento
+     * da rampa é feito com base em um valor constante e não colocando diretamente
+     * o valor dos controles nas saidas. Assim tanto as aceleradas quanto as
+     * frenagens
      * são suaves e evitam quedas
      * 
      */
@@ -129,32 +213,54 @@ public class Drivetrain extends SubsystemBase {
       if (rightOutput < right)
         rightOutput = right;
     }
-    if(reduce){
-      if(leftOutput>0 && leftOutput > DrivetrainConstants.kMaxSpeedArmExtended){
-        leftOutput = DrivetrainConstants.kMaxSpeedArmExtended;
-      }
-      if(leftOutput<0 && leftOutput < (-DrivetrainConstants.kMaxSpeedArmExtended)){
-        leftOutput = -DrivetrainConstants.kMaxSpeedArmExtended;
-      }
-      if(rightOutput>0 && rightOutput > DrivetrainConstants.kMaxSpeedArmExtended){
-        rightOutput = DrivetrainConstants.kMaxSpeedArmExtended;
-      }
-      if(rightOutput<0 && rightOutput < (-DrivetrainConstants.kMaxSpeedArmExtended)){
-        rightOutput = -DrivetrainConstants.kMaxSpeedArmExtended;
-      }
-      
-    }
 
-    if (reduce && leftOutput > DrivetrainConstants.kMaxSpeedArmExtended)
-      leftOutput = DrivetrainConstants.kMaxSpeedArmExtended;
-    if (reduce && rightOutput > DrivetrainConstants.kMaxSpeedArmExtended)
-      rightOutput = DrivetrainConstants.kMaxSpeedArmExtended;
     // see note above in robotInit about commenting these out one by one to set
     // directions.
     motorLeftRear.set(leftOutput);
     motorLeftFront.set(leftOutput);
     motorRightRear.set(rightOutput);
     motorRightFront.set(rightOutput);
+  }
+
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+  }
+
+  public void rateLimitedArcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    m_diffDrive.arcadeDrive(m_filter.calculate(xaxisSpeed), m_filter_turn.calculate(zaxisRotate));
+  }
+
+  /**
+   * The average distance in meters for both wheels
+   *
+   * @return The average distance in meters for both wheels
+   */
+  public double getAverageDistanceMeters() {
+    return (getLeftDistanceMeters() + getRightDistanceMeters()) / 2.0;
+  }
+
+  /**
+   * Controls the left and right sides of the drive directly with voltages.
+   * 
+   * @param leftVolts  the commanded left output
+   * @param rightVolts the commanded right output
+   */
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+
+    double rightVoltsCalibrated = rightVolts * DrivetrainConstants.rightVoltsGain;
+
+    // Send to Network Tables
+    m_leftVolts.setDouble(leftVolts);
+    m_rightVolts.setDouble(rightVoltsCalibrated);
+
+    // Apply the voltage to the wheels
+    m_leftMotor.setVoltage(leftVolts);
+    m_rightMotor.setVoltage(-rightVoltsCalibrated); // We invert this to maintain +ve = forward
+    m_diffDrive.feed();
+  }
+
+  public void stopDrivetrain() {
+    tankDriveVolts(0, 0);
   }
 
   public double getLeftPower() {
@@ -166,7 +272,7 @@ public class Drivetrain extends SubsystemBase {
   }
 
   // função de reset dos encoders definindo ponto inicial do robo
-  public void resetEncoder() {
+  public void resetEncoders() {
     leftEncoder1.setPosition(0);
     leftEncoder2.setPosition(0);
     rightEncoder1.setPosition(0);
@@ -180,24 +286,39 @@ public class Drivetrain extends SubsystemBase {
    * redução
    * e multiplicado pelo perimetro da roda para ter o resultado em metros
    */
-  public double getRightEncoder() {
+  public double getRightDistanceMeters() {
     return ((rightEncoder1.getPosition() + rightEncoder2.getPosition()) / 2 / DrivetrainConstants.kGearboxRatio)
-        * DrivetrainConstants.kWheelDistance;
+        * DrivetrainConstants.kWheelDiameterMeters * Math.PI;
+  }
+
+  public double getLeftDistanceMeters() {
+    return ((leftEncoder1.getPosition() + leftEncoder2.getPosition()) / 2 / DrivetrainConstants.kGearboxRatio)
+        * DrivetrainConstants.kWheelDiameterMeters * Math.PI;
+  }
+
+  public double getRightEncoder() {
+    return (rightEncoder1.getPosition() + rightEncoder2.getPosition()) / 2.0;
   }
 
   public double getLeftEncoder() {
-    return ((leftEncoder1.getPosition() + leftEncoder2.getPosition()) / 2 / DrivetrainConstants.kGearboxRatio)
-        * DrivetrainConstants.kWheelDistance;
+    return (leftEncoder1.getPosition() + leftEncoder2.getPosition()) / 2.0;
   }
-
+ /**
+     * Returns the currently estimated pose of the robot.
+     * @return The pose
+     */
+    public Pose2d getPose() {
+      return m_odometry.getPoseMeters();
+    }
+  
   // Captura o angulo que o robo está apontando
   public double getYaw() {
-    return navX.getYaw();
+    return m_gyro.getYaw();
   }
 
   // Zera a direção do robo
   public void resetYaw() {
-    navX.reset();
+    m_gyro.reset();
   }
 
   // Periodico só atualiza os dados no Dashboard para informações
@@ -206,6 +327,57 @@ public class Drivetrain extends SubsystemBase {
     SmartDashboard.putNumber("Distancia Esquerda", getLeftEncoder());
     SmartDashboard.putNumber("Distancia Direita", getRightEncoder());
     SmartDashboard.putNumber("Giro", getYaw());
-    SmartDashboard.putData(navX);
+    // Update the odometry in the periodic block
+    m_odometry.update(m_gyro.getRotation2d(),
+        getLeftDistanceMeters(),
+        getRightDistanceMeters());
+
+    m_poseEstimator.update(m_gyro.getRotation2d(),
+        getLeftDistanceMeters(),
+        getRightDistanceMeters());
+
+    // Also apply vision measurements. We use 0.3 seconds in the past as an example
+    // -- on
+    // a real robot, this must be calculated based either on latency or timestamps.
+    //m_poseEstimator.addVisionMeasurement(
+    //    //getLimelightPose(),
+    //    Timer.getFPGATimestamp() - 0.3);
+    publishTelemetry();
+
+  }
+  public void publishTelemetry() {
+      
+    m_field2d.setRobotPose(getPose());  
+    
+    m_estimatedField2d.setRobotPose(m_poseEstimator.getEstimatedPosition());
+  
+  
+    // Offset the pose to start 1.5 meters on the Y axis
+    // double yPoseOffset = 1.5;
+    // Pose2d currentPose = getPose();
+    // Pose2d poseOffset = new Pose2d(currentPose.getX(), 
+    //                                currentPose.getY() + yPoseOffset, 
+    //                                currentPose.getRotation());
+    // // Update the Field2D object (so that we can visualize this in sim)
+    // m_field2d.setRobotPose(poseOffset);
+
+    // Updates the the Unscented Kalman Filter using only wheel encoder information.
+    // m_estimator.update(m_gyro.getRotation2d(), 
+    //                   // getWheelSpeeds(), 
+    //                    m_leftEncoder.getDistance(), 
+    //                    m_rightEncoder.getDistance());
+
+
+    // // Offset the pose to start 1.5 meters on the Y axis
+    // Pose2d currentEstimatedPose = getEstimatedPose();
+    // Pose2d estimatedPoseOffset = new Pose2d(currentEstimatedPose.getX(), 
+    //                                         currentEstimatedPose.getY() + yPoseOffset, 
+    //                                         currentEstimatedPose.getRotation());
+
+    // // Update the Field2D object (so that we can visualize this in sim)
+    // m_estimatedField2d.setRobotPose(estimatedPoseOffset);
+
+    // Display the meters per/second for each wheel and the heading
+    
   }
 }
